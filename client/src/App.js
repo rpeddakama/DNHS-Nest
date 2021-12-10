efa2764b330039360f1f6d69653b3cae665e6ee6
rpeddakama <rishipeddakama@gmail.com>
2021-12-09 21:20:42 -0800

    delete uneeded files and create webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
+const App = () => {
+  const [clients, setClients] = useState()
+
+  useEffect(() => {
+    // const response = fetch("/clients")
+    // const body = await response.json()
+    // setClients(body)
+    // fetch("localhost:8080/clients").then((response) => setClients(response))
+    fetch("http://localhost:8080/clients")
+  })
+
+  console.log("CLIENTS", clients)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>WHATS MOFO</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
