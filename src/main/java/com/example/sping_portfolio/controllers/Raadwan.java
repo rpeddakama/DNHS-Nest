0542d0db8eadf4d8c78873514bd3ba83b17dc0d3
Raadwan Masum <piraadwan@gmail.com>
2021-12-15 10:16:48 -0800

    raadwan's unit 3 frq

--- src/main/java/com/example/sping_portfolio/controllers/Raadwan.java
+++ src/main/java/com/example/sping_portfolio/controllers/Raadwan.java
@@ -6,13 +6,24 @@ import java.net.http.*;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller  
 public class Raadwan {
     @GetMapping("/raadwan")
 
-    public String RaadwanAbout() {
+    public String RaadwanAbout(
+        @RequestParam(name="length", required=true, defaultValue="4") int length, 
+        @RequestParam(name="x", required=true, defaultValue="1") int x, 
+        @RequestParam(name="y", required=true, defaultValue="1") int y, 
+        Model model
+    ) {
+        model.addAttribute("length", length);
+        model.addAttribute("x", x);
+        model.addAttribute("y", y);
+
         return "raadwan"; 
     }
 
