c67c996e7e4862961a29d0ecc7a9c3a4aa70b604
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2021-12-15 21:48:17 -0800

    readjusted/updated unit 2 work

--- src/main/java/com/example/sping_portfolio/controllers/Arnav.java
+++ src/main/java/com/example/sping_portfolio/controllers/Arnav.java
@@ -4,14 +4,37 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
 
+import com.example.util.LightSequence;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Arnav {
     @GetMapping("/arnav")
-    public String ArnavAbout() {
+    public String ArnavAbout(
+            @RequestParam(name="segment", required=false, defaultValue="") String segment,
+            @RequestParam(name="a", required=false, defaultValue="0.0") double a,
+            @RequestParam(name="b", required=false, defaultValue="0.0") double b,
+            Model model
+    ) {
+        //Unit 2 FRQ
+        LightSequence gradShow = new LightSequence("0101 0101 0101");
+        gradShow.display();
+        gradShow.changeSequence("0011 0011 0011");
+        // String resultSeq = gradShow.insertSegment("1111 1111", 4);
+        String oldSequence = gradShow.toString();
+        String newSeq = oldSequence.replaceFirst(segment, "");
+        String addedSeq = oldSequence + " " + segment;
+
+        model.addAttribute("newSeq", newSeq);
+        model.addAttribute("addedSeq", addedSeq);
+
+        double answer = Math.sqrt(a*a + b*b);
+
+        model.addAttribute("answer", answer);
         return "arnav";
     }
 
