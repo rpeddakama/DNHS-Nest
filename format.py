613751a266ee58d637ce4bc3855a57942a56265d
rpeddakama <rishipeddakama@gmail.com>
2022-01-19 21:06:55 -0800

    python script to format terms quickly into markdown format

---
+++ format.py
@@ -0,0 +1,8 @@
+terms = []
+with open('terms.txt') as f:
+    for line in f:
+        terms.append(line.rstrip())
+
+with open("formatted.txt", "w") as f2:
+    for t in terms:
+        f2.write("(" + t + ")[] <br>\n")
