a443b52dd3f1781ae153ff8cf4f5f55089d4c790
rpeddakama <rishipeddakama@gmail.com>
2022-02-06 16:52:43 -0800

    unit 9 q1

--- src/main/java/com/example/sping_portfolio/controllers/Rishi.java
+++ src/main/java/com/example/sping_portfolio/controllers/Rishi.java
@@ -14,6 +14,52 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Random;
 import java.util.ArrayList;
 
+class BookListing {
+
+    private Book book;
+    private double price;
+
+    public BookListing(Book book, double price) {
+        this.book = book;
+        this.price = price;
+    }
+
+    public String printDescription() {
+        return (book.printBookInfo() + ", " + price);
+    };
+
+}
+
+class PictureBook extends Book {
+
+    private String illustrator;
+
+    public PictureBook(String t, String a, String i) {
+        super(t, a);
+        illustrator = i;
+    }
+
+    public String printBookInfo() {
+        return (super.title + ", written by " + super.author + ", illustrated by " + illustrator);
+    }
+}
+
+class Book {
+
+    public String title;
+    public String author;
+
+    public Book(String t, String a) {
+        title = t;
+        author = a;
+    }
+
+    public String printBookInfo() {
+        return (title + ", written by " + author);
+    }
+
+}
+
 class Plot {
     private String cropType;
     private int cropYield;
@@ -427,6 +473,15 @@ public class Rishi {
         model.addAttribute("highestYield",
                 (eF.getHighestYield(cropInput).getCropType() + ", " + eF.getHighestYield(cropInput).getCropYield()));
         model.addAttribute("sameCrop", eF.sameCrop(Math.min(columnInput, farmInput.length - 1)));
+
+        // Unit 9
+        ArrayList<Book> myLibrary = new ArrayList<Book>();
+        Book book1 = new Book("Frankenstein", "Mary Shelley");
+        Book book2 = new PictureBook("The Wonderful Wizard of Oz", "L. Frank Baum", "W.W. Denslow");
+        myLibrary.add(book1);
+        myLibrary.add(book2);
+        model.addAttribute("book1", book1.printBookInfo());
+        model.addAttribute("book2", book2.printBookInfo());
 
         return "rishi";
 
