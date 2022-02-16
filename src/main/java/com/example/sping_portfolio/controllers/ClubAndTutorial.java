e9f0937c9800c855eae08123a560837685b779e5
Arnav Palkhiwala <palkhiwala.arnav@gmail.com>
2022-02-16 10:03:34 -0800

    fix java file error

--- src/main/java/com/example/sping_portfolio/controllers/ClubAndTutorial.java
+++ src/main/java/com/example/sping_portfolio/controllers/ClubAndTutorial.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ClubAndTutorial {
     @GetMapping("/clubandtutorial")
-    public String ClubAndTutorial() {
+    public String ClubAndTutorialString() {
         return "clubandtutorial";
     }
 }
