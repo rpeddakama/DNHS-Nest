61c61d788aec17052522e6774ab3d92ae6f9046a
alvinzhengq <vortexdomainia@gmail.com>
2022-02-16 10:38:25 -0800

    bell schedule phase 1 mvp complete

--- src/main/java/com/example/sping_portfolio/controllers/BellSchedule.java
+++ src/main/java/com/example/sping_portfolio/controllers/BellSchedule.java
@@ -2,14 +2,17 @@ package com.example.sping_portfolio.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BellSchedule {
     @GetMapping("/bellschedule")
-    public String BellSchedule() {
+    public String Schedule() {
         return "bellschedule";
     }
+
+    // @GetMapping("/get-schedule")
+    // @ResponseBody
+    // public String GetSchedule(String schedule) {
+    //     return 
+    // }
 }
