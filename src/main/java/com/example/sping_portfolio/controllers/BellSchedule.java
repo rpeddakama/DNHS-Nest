7db4ef354a74271d73acda74464312764705c3a5
alvinzhengq <vortexdomainia@gmail.com>
2022-02-25 18:01:06 -0800

    added ical4j dependency

--- src/main/java/com/example/sping_portfolio/controllers/BellSchedule.java
+++ src/main/java/com/example/sping_portfolio/controllers/BellSchedule.java
@@ -3,6 +3,8 @@ package com.example.sping_portfolio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import net.fortuna.ical4j.model.*;
+
 @Controller
 public class BellSchedule {
     @GetMapping("/bellschedule")
