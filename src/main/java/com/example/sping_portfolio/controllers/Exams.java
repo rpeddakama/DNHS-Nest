cec933ec53c191eadfc4971bb6720f325bda7c47
Raadwan Masum <piraadwan@gmail.com>
2022-02-28 03:34:31 -0800

    add to database functionality

--- src/main/java/com/example/sping_portfolio/controllers/Exams.java
+++ src/main/java/com/example/sping_portfolio/controllers/Exams.java
@@ -6,16 +6,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller  
+import java.sql.*;
+
+@Controller
 public class Exams {
     @GetMapping("/exams")
     public String ExamsPage(
-        @RequestParam(name="examNameField", required=false, defaultValue="") String examNameField,
-        @RequestParam(name="classNameField", required=false, defaultValue="") String classNameField,
-        @RequestParam(name="teacherField", required=false, defaultValue="") String teacherField,
-        @RequestParam(name="classPeriodField", required=false, defaultValue="") String classPeriodField,
-        Model model
-    ) {
+            @RequestParam(name = "examNameField", required = false, defaultValue = "") String examNameField,
+            @RequestParam(name = "classNameField", required = false, defaultValue = "") String classNameField,
+            @RequestParam(name = "teacherField", required = false, defaultValue = "") String teacherField,
+            @RequestParam(name = "classPeriodField", required = false, defaultValue = "") String classPeriodField,
+            Model model) {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:exams.db");
+
+            stmt = c.createStatement();
+            String sql = "INSERT INTO EXAM (examName,className,teacher,classPeriod) " +
+                    "VALUES ('" + examNameField + "', '" + classNameField + "', '" + teacherField + "', '"
+                    + classPeriodField + "');";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        System.out.println("Opened database successfully");
+
         model.addAttribute("examName", examNameField);
         model.addAttribute("className", classNameField);
         model.addAttribute("teacher", teacherField);
